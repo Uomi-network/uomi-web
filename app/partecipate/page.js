c8b0b49b248e6dcdf80b1aea509415e74d6c0524
Gregorio Galante <me@gregoriogalante.com>
2025-09-02 17:17:24 +0200

    Page ready

---
+++ app/partecipate/page.js
@@ -0,0 +1,12 @@
+'use client'
+
+import Partecipate from "../../src/Pages/PartecipatePage";
+import AppWrapper from "../../src/components/AppWrapper";
+
+export default function Page() {
+  return (
+    <AppWrapper>
+      <Partecipate />
+    </AppWrapper>
+  );
+}
